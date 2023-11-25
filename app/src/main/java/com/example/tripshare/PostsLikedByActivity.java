4c935a9973d3d8655061183ed88afd6ad690d579
saraphtha <81167117+saraphtha@users.noreply.github.com>
2023-11-25 10:18:32 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/tripshare/PostsLikedByActivity.java
@@ -0,0 +1,14 @@
+package com.example.tripshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostsLikedByActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts_liked_by);
+    }
+}
