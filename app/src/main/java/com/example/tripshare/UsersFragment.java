50cdbc1cb9b1628ef40150177f990573618b6917
Bilalabdali1 <bilalabdali1234@gmail.com>
2023-11-26 01:49:08 -0500

    Update UsersFragment.java

--- app/src/main/java/com/example/tripshare/UsersFragment.java
+++ app/src/main/java/com/example/tripshare/UsersFragment.java
@@ -1,20 +1,35 @@
 package com.example.tripshare;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link UsersFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class UsersFragment extends Fragment {
-
+    RecyclerView recyclerView;
+    AdapterUsers adapterUsers;
+    List<ModelUsers> usersList;
+    FirebaseAuth firebaseAuth;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -45,7 +60,29 @@ public class UsersFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    private void getAllUsers() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    ModelUsers modelUsers = dataSnapshot1.getValue(ModelUsers.class);
+                    if (modelUsers.getUid() != null && !modelUsers.getUid().equals(firebaseUser.getUid())) {
+                        usersList.add(modelUsers);
+                    }
+                    adapterUsers = new AdapterUsers(getActivity(), usersList);
+                    recyclerView.setAdapter(adapterUsers);
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
